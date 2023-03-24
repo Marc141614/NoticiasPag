11c704dd6d50f0513c8713a91d7e3dd541a933c7
Marc141614 <rdz.marco.a@outlook.com>
2023-03-24 08:29:41 -0600

    24.3.2023

--- inicio.php
+++ inicio.php
@@ -269,7 +269,7 @@
 				}
 			}
 		</script>
-		<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-		<script src="js/alert.js"></script>
+		<!--<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+		<script src="js/alert.js"></script>-->
 	</body>
-</html>	
+</html>
--- js/alert.js
+++ js/alert.js
@@ -1,3 +1,6 @@
-Swal.fire({
-    title: "Hola"
-})
+const btnAceptar = document.getElementById('btn-aceptar');
+
+btnAceptar.addEventListener('click', () => {
+	// Aquí se pueden colocar las acciones que se deseen al aceptar los términos y condiciones
+	alert('¡Gracias por aceptar los términos y condiciones!');
+});
